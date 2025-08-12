f8239214df5fb2413f3f065d9bb15cc9bdbbd6d0
stampini81 <stampini81@gmail.com>
2025-08-12 01:36:24 +0000

    uiman.java commit

--- src/br/com/dio/UIMain.java
+++ src/br/com/dio/UIMain.java
@@ -9,6 +9,16 @@ import static java.util.stream.Collectors.toMap;
 public class UIMain {
 
     public static void main(String[] args) {
+        // --- INÍCIO DA CORREÇÃO ---
+        try {
+            System.out.println("Aguardando o ambiente gráfico iniciar (pausa de 3 segundos)...");
+            Thread.sleep(3000); // Pausa por 3000 milissegundos (3 segundos)
+            System.out.println("Ambiente pronto. Iniciando a UI do Sudoku.");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        // --- FIM DA CORREÇÃO ---
+
         final var gameConfig = Stream.of(args)
                 .collect(toMap(k -> k.split(";")[0], v -> v.split(";")[1]));
         var mainsScreen = new MainScreen(gameConfig);
